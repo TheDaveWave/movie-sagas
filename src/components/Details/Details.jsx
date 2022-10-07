81a2eecda0e8532f48cc5b5ffb0008b2dae47d1e
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-07 16:22:00 -0500

    exported the details and movieform components

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,12 @@
+
+function Details() {
+    return (
+        <section>
+            <div>
+                <h1>This is the details page.</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Details;
---
+++ src/components/MovieForm/MovieForm.jsx
@@ -0,0 +1,12 @@
+
+function MovieForm() {
+    return (
+        <section>
+            <div>
+                <h1>This is the Movie Form!</h1>
+            </div>
+        </section>
+    );
+}
+
+export default MovieForm;
