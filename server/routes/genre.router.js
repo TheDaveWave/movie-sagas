3e793565cd982815e6c205ff7118cc782047e594
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-08 16:36:17 -0500

    created succesful get request to get genres for a specific movie

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -16,4 +16,28 @@ router.get('/', (req, res) => {
   })
 });
 
+// GET all genres for a specified movie id using JOINS.
+router.get('/:movieId', (req, res) => {
+  // set movieId to req.params.movieId
+  const movieId = req.params.movieId;
+  // setup SQL query statement.
+  const queryText = `SELECT "mg"."id",
+  "mg"."genre_id",
+  "g"."name" AS "genre",
+  "mg"."movie_id"
+  FROM "genres" AS "g"
+  JOIN "movies_genres" AS "mg" ON "g"."id"="mg"."genre_id"
+  JOIN "movies" AS "m" ON "mg"."movie_id"="m"."id"
+  WHERE "m"."id"=$1;`;
+
+  pool.query(queryText, [movieId])
+  .then(response => {
+    res.send(response.rows);
+  })
+  .catch(err => {
+    console.log('Error in GET for genres of movie', err);
+    res.sendStatus(500);
+  });
+});
+
 module.exports = router;
