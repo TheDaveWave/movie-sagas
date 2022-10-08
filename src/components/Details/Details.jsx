a23cf93a016692eaacf049be2897432000f908df
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-08 16:51:27 -0500

    created a dispatch to display the genres for the movie on the details page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,16 +5,22 @@ import { useParams } from "react-router-dom";
 function Details() {
     // get the current store of movie objects from the movies reducer.
     const movie = useSelector(store => store.movieDetails);
+    const movieGenres = useSelector(store => store.movieGenres);
     // setup dispatch from useDispatch()
     const dispatch = useDispatch();
     // get the movie id from the route url params
     let { movieId } = useParams(); 
     // console.log(movieId);
 
-    // fetch the movie with the movieId from the params on load.
     useEffect(() => {
+        // fetch the movie with the movieId from the params on load.
         dispatch({
             type: 'GET_MOVIE_DEETS',
+            payload: Number(movieId)
+        });
+        // fetch the genres for the movieId on load.
+        dispatch({
+            type: 'GET_MOVIE_GENRES',
             payload: Number(movieId)
         });
     }, []);
@@ -32,6 +38,10 @@ function Details() {
                 <h2>{movie.title}</h2>
                 <img src={movie.poster}/>
                 <p>{movie.description}</p>
+                <p>Genres:</p>
+                {movieGenres.map(name => (
+                    <p>{name.genre}</p>
+                ))}
             </div>
         </section>
     );
